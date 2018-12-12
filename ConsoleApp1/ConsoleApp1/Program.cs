b686a1cd6153cb81cf38c8223dad1a0449e36726
Enki502 <45813387+Enki502@users.noreply.github.com>
2018-12-12 13:58:15 +0000

    Add files via upload
    Sprint Cycle 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SessionController Controller = new SessionController();
+            Controller.Initialise();
+
+            Console.ReadKey();
+        }
+    }
+        
+    class SessionController
+    {
+        public SessionController()
+        {
+            
+        }
+        public void Initialise()
+        {
+            string UserTitle ="";
+            Menu(UserTitle);
+            OMDbInterface OMDbInterface = new OMDbInterface();
+            JObject FilmData = OMDbInterface.GetDataRecords(UserTitle);
+
+
+        }
+
+        public void Menu(string UserTitle)
+        {
+            Console.WriteLine("Enter movie title");
+            UserTitle = Console.ReadLine();
+        }
+
+        public void DisplayFilmInfo(JObject FilmData)
+        {
+            foreach (JProperty property in FilmData.Properties())
+            {
+                Console.WriteLine(property.Name + " - " + property.Value);
+            }
+        }
+    }
+
+    class OMDbInterface
+    {
+        public OMDbInterface()
+        {
+
+        }
+
+        public JObject GetDataRecords(string Title)
+        {
+            return JObject.Parse(GET("http://www.omdbapi.com/?t=" + Title + "&apikey=c5f5153e"));
+        }
+
+        string GET(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+
+
+            //try
+            //{
+            //    WebResponse response = request.GetResponse();
+            //    using (Stream responseStream = response.GetResponseStream())
+            //    {
+            //        StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+            //        return reader.ReadToEnd();
+            //    }
+            //}
+            //catch (WebException ex)
+            //{
+            //    WebResponse errorResponse = ex.Response;
+            //    using (Stream responseStream = errorResponse.GetResponseStream())
+            //    {
+            //        StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+            //        String errorText = reader.ReadToEnd();
+            //        // log errorText
+            //    }
+            //    throw;
+        }
+    }
+
+    class DisplayFilmProperties
+    {
+        public DisplayFilmProperties()
+        {
+        }
+
+    }
+
+
+
+}
